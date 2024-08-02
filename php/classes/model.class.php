ca3575013118ece7bfef19907f48480349aeb839
PetoB000 <peto.b000@gmail.com>
2024-08-02 22:38:41 +0200

    nnna

--- php/classes/model.class.php
+++ php/classes/model.class.php
@@ -3,9 +3,9 @@
 class Dbh {
     public function connect() {
         try {
-            $username = "root";
-            $password = "";
-            $dbh = new PDO('mysql:host=127.0.0.1;dbname=webshop', $username, $password);
+            $username = "prooktat_PB";
+            $password = "argovk;tRG8v";
+            $dbh = new PDO('mysql:host=127.0.0.1;dbname=prooktat_PB', $username, $password);
             return $dbh;
         } catch (PDOException $e) {
             print("error!: " . $e->getMessage());
